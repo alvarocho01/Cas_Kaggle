eddb7d8447a1a0f185f17806380f71c507b9aa97
alvarocho01 <alvarobcn02@gmail.com>
2021-12-07 18:55:08 +0100

    mes canvis

--- code/models.py
+++ code/models.py
@@ -38,7 +38,7 @@ title_y = titles[11]
 
 
 
-
+"""
 particions = [0.5, 0.8, 0.7]
 models = [svm.SVC(probability=True), Perceptron(), KNeighborsClassifier(), DecisionTreeClassifier(), RandomForestClassifier(), LogisticRegression()]
 nom_models = ["Support Vector Machines", "Perceptron", "KNN", "Decision Tree", "Random Forest", "Logistic Regression"]
@@ -55,3 +55,41 @@ for i,model in enumerate(models):
         scores = cross_val_score(model, x, y, cv=k)
         print("Score promig amb k-fold = ", k, " : ", scores.mean())
     print("")
+    
+"""  
+
+param_svm = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001],'kernel': ['rbf', 'poly', 'sigmoid']}
+param_perceptron = {'penalty': ['l2','l1','elasticnet'], 'alpha': [0.0001, 0.001, 0.01, 0.1, 1],
+                    'fit_intercept': [True, False], 'shuffle': [True, False]}
+param_knn = { 'n_neighbors' : [5,7,9,11,13,15],
+               'weights' : ['uniform','distance'],
+               'metric' : ['minkowski','euclidean','manhattan']}
+param_decisiontree = {
+    'max_depth': [2, 3, 5, 10, 20],
+    'min_samples_leaf': [5, 10, 20, 50, 100],
+    'criterion': ["gini", "entropy"]
+}
+param_randomforest = {
+    'bootstrap': [True],
+    'max_depth': [80, 90, 100, 110],
+    'max_features': [2, 3],
+    'min_samples_leaf': [3, 4, 5],
+    'min_samples_split': [8, 10, 12],
+    'n_estimators': [100, 200, 300, 1000]
+}
+param_logireg = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000] }
+
+param_grid = [param_perceptron, param_knn, param_decisiontree, param_randomforest, param_logireg]
+
+models = [Perceptron(), KNeighborsClassifier(), DecisionTreeClassifier(), RandomForestClassifier(), LogisticRegression()]
+nom_models = ["Support Vector Machines", "Perceptron", "KNN", "Decision Tree", "Random Forest", "Logistic Regression"]
+
+for i,model in enumerate(models):
+    '''Busqueda exhaustiva de los mejores parametros'''
+    print("BUSQUEDA EXHAUSTIVA DE PARAMETROS")
+    grid = GridSearchCV(model, param_grid[i], verbose=3, n_jobs=-1)
+    grid.fit(x,y)
+    print("Els millors parametres: ",grid.best_params_)
+    print("El millor score: ", grid.best_score_)
+    print("")
+
